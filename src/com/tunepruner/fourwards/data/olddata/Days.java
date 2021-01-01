5d9700c40a5c8c18e668bde9cbfac7860cd1b79c
Philip Carlson <writephilcarlson@gmail.com>
2020-12-31 20:37:12 -0500

    Deleted Days class

--- src/com/tunepruner/fourwards/data/olddata/Days.java
+++
@@ -1,9 +0,0 @@
-package com.tunepruner.fourwards.data.olddata;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class Days {
-//    private static ObservableList<Day> days = FXCollections.observableArrayList();
-
-}
